d3fb5e625de06b47b046f962398c085aaa0e8509
Winga <andywinga@hotmail.be>
2024-11-18 15:00:00 +0100

    Ajout de la prise en charge d'Alpine.js, mise à jour des dépendances, et création de nouveaux composants Blade pour l'interface utilisateur

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,10 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth'])->group(function () {
-    Route::resource('users', UserController::class);
-});
+require __DIR__ . '/auth.php';
